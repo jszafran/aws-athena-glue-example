19538e27836fae04cb3895943b133544a3d0b68e
jszafran <jszafran.pv@gmail.com>
2021-04-16 16:50:17 +0200

    Fmting s3 file.

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "input_data_bucket" {
 
 resource "aws_s3_bucket" "athena_results_bucket" {
   bucket = "jsz-tf-athena-results-bucket"
-  acl = "private"
+  acl    = "private"
 
   tags = {
     Project = "AWS Athena Glue Terraform example"
