70385506c35b73f74e5a56f9ad7961918107e09a
jszafran <jszafran.pv@gmail.com>
2021-04-26 11:36:51 +0200

    Adding additional statements to IAM policy.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -22,33 +22,61 @@ resource "aws_iam_policy" "athena_read_run_query_policy" {
     Statement = [
       {
         Action = [
-          "ListDatabases",
-          "ListWorkgroups",
-          "GetDataCatalog",
-          "ListEngineVersions"
+          "athena:ListDatabases",
+          "athena:ListWorkgroups",
+          "athena:GetDataCatalog",
+          "athena:ListEngineVersions"
         ],
         Effect   = "Allow"
         Resource = "arn:aws:athena:*:584229380385:datacatalog/*"
       },
       {
         Action = [
-          "ListWorkgroups"
+          "athena:ListWorkGroups"
         ]
         Effect   = "Allow"
         Resource = "*"
       },
       {
         Action = [
-          "GetQueryExecution",
-          "GetQueryResults",
-          "GetQueryResultsStream",
-          "GetWorkgroup",
-          "ListQueryExecutions",
-          "StartQueryExecution",
-          "StopQueryExecution"
+          "athena:GetQueryExecution",
+          "athena:GetQueryResults",
+          "athena:GetQueryResultsStream",
+          "athena:GetWorkGroup",
+          "athena:ListQueryExecutions",
+          "athena:StartQueryExecution",
+          "athena:StopQueryExecution"
         ]
         Effect   = "Allow"
         Resource = aws_athena_workgroup.athena_example_workgroup.arn
+      },
+      {
+        Action = [
+          "glue:GetDatabases",
+          "glue:GetDatabase",
+          "glue:GetTables",
+          "glue:GetTable"
+        ],
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:glue:eu-north-1:584229380385:catalog",
+          aws_glue_catalog_database.example_db.arn,
+          aws_glue_catalog_table.products.arn,
+        ]
+      },
+      {
+        Action = [
+          "s3:ListBucket"
+        ]
+        Effect   = "Allow"
+        Resource = aws_s3_bucket.input_data_bucket.arn
+      },
+      {
+        Action = [
+          "s3:GetObject"
+        ]
+        Effect   = "Allow"
+        Resource = "${aws_s3_bucket.input_data_bucket.arn}/*"
       }
     ]
   })
@@ -56,6 +84,6 @@ resource "aws_iam_policy" "athena_read_run_query_policy" {
 
 resource "aws_iam_policy_attachment" "athena_business_user_attachment" {
   name       = "athena_business_user_attachment"
-  roles      = [aws_iam_role.athena_business_user_role]
+  roles      = [aws_iam_role.athena_business_user_role.name]
   policy_arn = aws_iam_policy.athena_read_run_query_policy.arn
 }
